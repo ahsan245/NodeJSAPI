d1530ddfeee2626a6af2001678c154f32b45d498
Muhammad Ahsan <muhammadahsanty@outlook.com>
2022-12-17 16:47:57 +0500

    Eps 2 Modification

--- models/category.model.js
+++ models/category.model.js
@@ -1,23 +1,33 @@
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 
 const category = mongoose.model(
     "Category",
     mongoose.Schema({
-        categoryName:{
-            type:String,
-            required:true,
-            unique:true,
+        categoryName: {
+            type: String,
+            required: true,
+            unique: true,
         },
-        categoryDescription:{
-            type:String,
-            required:false
+        categoryDescription: {
+            type: String,
+            required: false
         },
-        categoryImage:{
-            type:String,
+        categoryImage: {
+            type: String,
         },
-    })
+    },
+        {
+            toJSON: {
+                transform: function (doc, ret) {
+                    ret.categoryId = ret._id.toString();
+                    delete ret._id;
+                    delete ret._v;
+                },
+            },
+        }
+    )
 );
 
-module.exports={
+module.exports = {
     category,
 };
