05c31163cb2e985472659bd6d5cd478f1e9587a5
Muhammad Ahsan <muhammadahsanty@outlook.com>
2022-12-18 14:59:32 +0500

    Final Edit

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const { MONGO_DB_CONFIG } = require("./config/app.config");
 const errors = require("./middleware/errors.js");
 const swaggerUi = require("swagger-ui-express"), swaggerDocument = require("./swagger.json");
 
+
+
+
 mongoose.Promise = global.Promise;
 mongoose
     .connect(MONGO_DB_CONFIG.DB, {
@@ -27,9 +30,12 @@ app.use("/api",require("./routes/app.routes"));
 app.use(errors.errorHandler);
 app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerDocument));
 
+
+
 app.listen(process.env.port || 4000,function(){
     console.log("Ready to Go!");
 
 
 
 });
+
