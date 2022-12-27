623b656608f407c3fd1bd3afdd471587a927804c
Muhammad Ahsan <muhammadahsanty@outlook.com>
2022-12-27 13:55:27 +0500

    cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const { MONGO_DB_CONFIG } = require("./config/app.config");
 const errors = require("./middleware/errors.js");
+const cors = require("cors");
 const swaggerUi = require("swagger-ui-express"), swaggerDocument = require("./swagger.json");
 
 
@@ -23,7 +24,9 @@ mongoose
             console.log("Database can't be connected:" + error);
         }
     );
-
+app.use(cors({
+    origin:"*"
+}));
 app.use(express.json());
 app.use("/uploads",express.static("uploads"));
 app.use("/api",require("./routes/app.routes"));
