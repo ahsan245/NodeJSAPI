e3a657369fdec0c9322e56f1c51706bc3d04fdd5
Muhammad Ahsan <muhammadahsanty@outlook.com>
2023-05-15 12:20:14 +0500

    Added Password Reset through Email

--- services/users.service.js
+++ services/users.service.js
@@ -226,7 +226,7 @@ function createEmailOtp(params, callback) {
 
     // Send email with OTP
     const mailOptions = {
-        from: "Theek Karo",
+        from: "no-reply@theekkaro.tech",
         to: params.email,
         subject: "Your OTP for Theek-Karo",
         text: `${otp} is your Theek-Karo OTP. Do not share it with anyone.`
