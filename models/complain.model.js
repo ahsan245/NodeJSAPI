2975317f980bc0693f2c7bc9b0af56b32a94dada
Muhammad Ahsan <muhammadahsanty@outlook.com>
2023-07-02 14:35:35 +0500

    Date

--- models/complain.model.js
+++ models/complain.model.js
@@ -1,100 +1,104 @@
 const mongoose = require("mongoose");
 
-const complainSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-    },
-    complainName: {
-        type: String,
-        required: true,
-    },
-    complainDescription: {
-        type: String,
-        required: false
-    },
-    complainCategory: {
-        type: String,
-        required: false
-    },
-    categoryassigned: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Category",
-        required: false
-    },
-    assignedTech: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Tech",
-        required: false
-    },
-    userAddress: {
-        type: String,
-        required: true
-    },
-    complainStatus: {
-        type: Boolean,
-        default: false
-    },
-    userContact: {
-        type: String,
-        required: false
-    },
-    complainCheckList: [{ type: String }],
-    complainImage: {
-        type: String,
-        required: false
-    },
-    longitude: {
-        type: String,
-        required: false
-    },
-    latitude: {
-        type: String,
-        required: false
-    },
-    techComment: {
-        type: String,
-        required: false
-    },
-    completeUpdate: {
-        type: Boolean,
-        default: false
-    },
-    refBill: {
-        type: String,
-        required: false
-    },
-    paymentStatus: {
-        type: Boolean,
-        default: false
-    },
-    billAmount: {
-        type: String,
-        required: false
-    },
-    startComplain: {
-        type: Boolean,
-        default: false
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: function () {
-            return this._createdAt.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }) + ' ' + this._createdAt.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric' });
-        }
-    }
-},
-    {
-        toJSON: {
-            transform: function (doc, ret) {
-                ret.complainId = ret._id.toString();
-                delete ret._id;
-                delete ret.__v;
-            },
+const complain = mongoose.model(
+    "complains",
+    mongoose.Schema({
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
         },
-    });
+        complainName: {
+            type: String,
+            required: true,
+        },
+        complainDescription: {
+            type: String,
+            required: false
+        },
+        complainCategory: {
+            type: String,
+            required: false
+        },
+        categoryassigned: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Category",
+            required: false
+        },
+        assignedTech: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Tech",
+            required: false
+        },
+        userAddress: {
+            type: String,
+            required: true
+        },
+        complainStatus: {
+            type: Boolean,
+            default: false
+        },
+        userContact: {
+            type: String,
+            required: false
+        },
+        complainCheckList: [{ type: String }],
+        complainImage: {
+            type: String,
+            required: false
+        },
+        longitude: {
+            type: String,
+            required: false
+        },
+        latitude: {
+            type: String,
+            required: false
+        },
+        techComment: {
+            type: String,
+            required: false
+        },
+        completeUpdate: {
+            type: Boolean,
+            default: false
+        },
+        refBill: {
+            type: String,
+            required: false
+        },
+        paymentStatus: {
+            type: Boolean,
+            default: false
+        },
+        billAmount: {
+            type: String,
+            required: false
+        },
+        startComplain: {
+            type: Boolean,
+            default: false
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: function () {
+                return this.createdAt.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }) + ' ' + this.createdAt.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric' });
+            }
+        }
 
-const complain = mongoose.model("complains", complainSchema);
+
+    },
+        {
+            toJSON: {
+                transform: function (doc, ret) {
+                    ret.complainId = ret._id.toString();
+                    delete ret._id;
+                    delete ret.__v;
+                },
+            },
+        }
+    )
+);
 
 module.exports = {
     complain,
