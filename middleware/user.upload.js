4d501b2218ca91d0081d8101d58b20d1e6c94d9c
Muhammad Ahsan <muhammadahsanty@outlook.com>
2023-07-03 19:17:16 +0500

    Image

--- middleware/user.upload.js
+++ middleware/user.upload.js
@@ -1,34 +1,37 @@
 const multer = require("multer");
-const Path = require("path");
+const path = require("path");
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./uploads/users");
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + "-" + file.originalname);
+
+
+const storage = multer.diskStorage(
+    {
+        destination: function (req, file, cb) {
+            cb(null, "./uploads/users");
+        },
+        filename: function (req, file, cb) {
+            cb(null, Date.now() + "-" + file.originalname);
+        }
     }
-
-});
-
+);
 
 const fileFilter = (req, file, callback) => {
-    const acceptableExt = [".png", ".jpg", ".jpeg"];
-    if (!acceptableExt.includes(Path.extname(file.originalname))) {
-
+    const validExts = [".png", ".jpg", ".jpeg"];
+    if (!validExts.includes(path.extname(file.originalname))) {
+        return callback(new Error("Only .png, .jpg and .jpeg format allowed"));
     }
     const fileSize = parseInt(req.headers["content-length"]);
+
     if (fileSize > 1048576) {
         return callback(new Error("File Size Big"));
     }
 
     callback(null, true);
-};
+}
 
 let upload = multer({
     storage: storage,
     fileFilter: fileFilter,
-    fileSize: 1048576,
+    fileSize: 1048576
 });
 
 module.exports = upload.single("userImage");
