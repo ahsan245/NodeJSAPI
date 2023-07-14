542a20c94f66a066a8941198a818da5c5c56b303
Muhammad Ahsan <muhammadahsanty@outlook.com>
2023-07-14 21:30:23 +0500

    Remove Auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,18 +3,16 @@ const jwt = require("jsonwebtoken");
 
 const TOKEN_KEY = "RANDOM_KEY";
 
-function authenticationToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+const authHeader = req.headers['authorization'];
+const token = authHeader && authHeader.split(' ')[1];
 
-    if (token == null) return res.sendStatus(401);
+if (token == null) return res.sendStatus(401);
 
-    jwt.verify(token, TOKEN_KEY, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user;
-        next();
-    });
-}
+jwt.verify(token, TOKEN_KEY, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+});
 
 function generateAccessToken(userModel) {
     return jwt.sign({ data: userModel }, TOKEN_KEY, {
